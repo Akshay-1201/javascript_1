cbd145ebc9c658d1cd9cc0b3e1cceb5948cd7cb4
Akshay-1201 <akshaykute12@gmail.com>
2023-02-07 21:45:08 +0530

    first commit

---
+++ 2_interpolation.js
@@ -0,0 +1,14 @@
+// 02. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+// Ans: interpolation method is used with literals ${}
+
+const firstName = "Akshay";
+const lastName = "Kute";
+let maritalStatus ='Single';
+let country = 'India';
+let age = 24 ; 
+
+console.log(` My name is ${firstName} ${lastName}.
+ I am ${age} years old. 
+ currently I am ${maritalStatus}.
+ I live in ${country}.`);
