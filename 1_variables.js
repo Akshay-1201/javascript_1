a794b36a67266e8c232db5b919b4715a78fcac5f
Akshay-1201 <akshaykute12@gmail.com>
2023-02-07 21:37:27 +0530

    first commit

---
+++ 1_variables.js
@@ -0,0 +1,19 @@
+// 01. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+//  "typeof" this is the keyword that we use to find the type of value, variable is holding.
+
+// => console.log is used to print the output on the screen.
+let  userName = 'Akshay Kute';
+console.log("Datatype of", userName , "is " , typeof userName );
+
+let number = 12
+console.log("Datatype of", number , "is " , typeof number );
+
+let var2 = true;
+console.log("Data type of", var2 , "is" , typeof var2);
+
+let century;
+console.log("Data type of course is" , typeof course);
+
+let marks = null;
+console.log("Data type of marks is", typeof marks);
