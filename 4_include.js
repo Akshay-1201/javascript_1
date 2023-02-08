cd8edb5de3ebe6965819787c6df45346b3794e2b
Akshay-1201 <akshaykute12@gmail.com>
2023-02-08 15:32:00 +0530

    commit

---
+++ 4_include.js
@@ -0,0 +1,9 @@
+// Declare a varibale and assign string value to it and check if the string 
+//contains a word Script using includes() method.
+//If the condition contains that word it wii show the output as true 
+let a = "INDIA is very beautiful country";
+
+console.log(a.includes('bea'));
+//If it dosent contains it will show the false
+
+console.log(a.includes('aks'));
