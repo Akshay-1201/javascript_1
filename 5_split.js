4328166d1973b850d9410feb30a440194dc0f14a
Akshay-1201 <akshaykute12@gmail.com>
2023-02-08 15:44:13 +0530

    commit

---
+++ 5_split.js
@@ -0,0 +1,11 @@
+// 05. Declare a varibale and assign string value to it and then split it into 
+//an array using split() method
+// for spliting the words give space in the quotes(" ") 
+let text = "How are you doing today?";
+const myArray = text.split(" ");
+console.log(myArray);
+
+// to split all letters dont give space("")
+
+let a = "akshay";
+console.log(a.split(""));
