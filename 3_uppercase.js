acc994095b1df273ec5801c7bfe9d099529a012a
Akshay-1201 <akshaykute12@gmail.com>
2023-02-08 15:24:10 +0530

    commit

---
+++ 3_uppercase.js
@@ -0,0 +1,8 @@
+// 03. Declare a varibale and assign string value to it and change all the 
+//string characters to capital letters using toUpperCase() method.
+let a = "akshay";
+console.log(a.toUpperCase());
+// another way to do it
+let text = "Hello World!";
+let result = text.toUpperCase();
+console.log(result);
