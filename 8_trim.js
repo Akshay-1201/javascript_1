85fd87e9556f6eeeee68ebf576016d9c78790039
Akshay-1201 <akshaykute12@gmail.com>
2023-02-09 15:39:53 +0530

    first commit

---
+++ 8_trim.js
@@ -0,0 +1,7 @@
+// 08. Demonstrate the use of trim() to remove any trailing whitespace at the beginning and the end of a string.
+//before trim
+let a = "    akshay kute     ";
+console.log("with whitespace",a,".");
+//afer using trim to remove whitwspace
+
+console.log("without whitespace",a.trim(),".");
---
+++ 9_boolean.js
@@ -0,0 +1,11 @@
+//09. Boolean value is either true or false.
+    //- Write three JavaScript statement example which provide truthy value.
+    //- Write three JavaScript statement example which provide falsy value.
+let a = "student"
+if(a){
+    a = "student"
+    console.log(Boolean)
+}
+else{
+    console.log(Boolean)
+}
