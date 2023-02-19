e7b65290a527f87d72c740063115182f00866bc2
Akshay-1201 <akshaykute12@gmail.com>
2023-02-19 21:53:50 +0530

    first commit

---
+++ 28_pattern.js
@@ -0,0 +1,27 @@
+// 28. Write a program to print the given patterns using the loops-
+// 	a. Print a triangle pattern, if the given input is 3 then the pattern should be similar to the given output
+// 	   *
+// 	   **
+// 	   ***
+
+const { pause } = require("prompt");
+
+	
+// 	b. Print a square pattern, if the input is 3 then the output should be similar to the given output
+// 	   ***
+// 	   ***
+// 	   ***
+
+// 	c. Print a pyramid pattern, if the input is 3 then the output should be similar to the given output
+// 	     *
+// 	    ***
+
+let n = 3;
+let string = "";
+for (let i = 1; i <= n; i++) {
+  for (let j = 0; j < i; j++) {
+    string += "*";
+  }
+  string += "\n";
+}
+console.log(string);
